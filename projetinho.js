857faf6da8165b08f2fb003a285fb68b3bcb84ad
breno birschener <brenobirschener@gmail.com>
2023-11-03 21:33:33 -0300

    projeto criado

---
+++ projetinho.js
@@ -0,0 +1,28 @@
+// Projetinho do Felipao da Dio.
+
+let nomeHeroi = "The Green Arrow"
+let xpHeroi = 15000
+
+if(xpHeroi <= 1000) {
+
+console.log("O Herói " + nomeHeroi + " possui nível de Ferro: " + xpHeroi + " pontos de vida.")
+
+} else if((xpHeroi > 1000) && (xpHeroi <= 2000)){
+    console.log("O Herói " + nomeHeroi + " possui nível de Bronze: " + xpHeroi + " pontos de vida.")
+} else if((xpHeroi > 2000) && (xpHeroi <= 5000)){
+    console.log("O Herói " + nomeHeroi + " possui nível de Prata: " + xpHeroi + " pontos de vida.")
+} else if((xpHeroi > 5000) && (xpHeroi <= 7000)){
+    console.log("O Herói " + nomeHeroi + " possui nível de Ouro: " + xpHeroi + " pontos de vida.")
+} else if((xpHeroi > 7000) && (xpHeroi <= 8000)){
+    console.log("O Herói " + nomeHeroi + " possui nível de Platina: " + xpHeroi + " pontos de vida.")
+} else if((xpHeroi > 8000) && (xpHeroi <= 9000)){
+    console.log("O Herói " + nomeHeroi + " possui nível de Ascendente: " + xpHeroi + " pontos de vida.")
+} else if((xpHeroi > 9000) && (xpHeroi <= 10000)){
+    console.log("O Herói " + nomeHeroi + " possui nível de Imortal: " + xpHeroi + " pontos de vida.")
+} else if((xpHeroi >= 10001) ){
+    console.log("O Herói " + nomeHeroi + " possui nível Radiante: " + xpHeroi + " pontos de vida.")
+}
+
+
+   
+
